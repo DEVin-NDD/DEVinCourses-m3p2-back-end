0861dc96a76f2415023236286d7ba79a9d447088
Lucas <lucasdjs2014@hotmail.com>
2022-11-15 17:38:46 -0300

    feat: criação de interfaces de serviço

--- NDDTraining.Domain/Interfaces/Services/IModuleService.cs
+++ NDDTraining.Domain/Interfaces/Services/IModuleService.cs
@@ -1,12 +1,12 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace NDDTraining.Domain.Interfaces.Services
 {
-    public interface Placeholder
+    public interface IModuleService
     {
-        
     }
 }
