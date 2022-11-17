8bac2044146acd234fcda0d5f01300b3104edc9e
Vitor Serrão <vitorwss@gmail.com>
2022-11-17 20:51:47 -0300

    Injetar as dependências entre os serviços/repositórios e suas respectivas interfaces.

--- Src/NDDTraining.DI/IOC/DI.cs
+++ Src/NDDTraining.DI/IOC/DI.cs
@@ -1,15 +1,20 @@
 ﻿using Microsoft.Extensions.DependencyInjection;
 using NDDTraining.Domain.Interfaces.Services;
 using NDDTraining.Domain.Services;
-
+using NDDTraining.Domain.Interfaces.Repositories;
+using NDDTraining.Infra.Data.Repository;
 namespace NDDTraining.DI.IOC
 {
     public static class DI
     {
         public static IServiceCollection RegisterServices(this IServiceCollection services)
         {
-            return services
-                .AddScoped<IModuleService, ModuleService>();
+            
+                services.AddScoped<IModuleService, ModuleService>(); 
+                services.AddScoped<IUserRepository, UserRepository>();
+                services.AddScoped<IUserService, UserService>();
+
+            return services;
         }
     }
 }
--- Src/NDDTraining.Infra/Data/Repository/UserRepository.cs
+++ Src/NDDTraining.Infra/Data/Repository/UserRepository.cs
@@ -1,10 +1,17 @@
 ﻿using NDDTraining.Domain.Models;
 using NDDTraining.Infra.Data.Context;
+using NDDTraining.Domain.Interfaces.Repositories;
 
 namespace NDDTraining.Infra.Data.Repository
 {
-    public class UserRepository : BaseRepository<User, int>
+    public class UserRepository : BaseRepository<User, int>, IUserRepository
     {
         public UserRepository(NDDTrainingDbContext context) : base(context) { }
+
+        public User GetByToken(string id)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
+
