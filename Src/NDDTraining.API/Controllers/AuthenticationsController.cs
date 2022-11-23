6df6c17ae44a4c6476d898fd9f069363a65fc677
Rodrigo Rosa <rodrigorosa.dev@gmail.com>
2022-11-22 21:59:14 -0300

    Add files
    Add files

--- Src/NDDTraining.API/Controllers/AuthenticationsController.cs
+++ Src/NDDTraining.API/Controllers/AuthenticationsController.cs
@@ -8,7 +8,6 @@ namespace NDDTraining.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
-    [Authorize]
     public class AuthenticationsController : Controller
     {
         private readonly IUserService _userService;
